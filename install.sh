deab8d9f8d0369a3751492e324eab505bab5eaf3
Liang-Shih Lin <liangshihlin@gmail.com>
2025-11-25 21:28:55 +0800

    Updated

--- install.sh
+++ install.sh
@@ -5,26 +5,29 @@ cp -R .zshrc .tmux.conf .bash_aliases .config/ .scripts .local ~/
 echo "Done: Copying Configs"
 
 echo "Downloading Terminal Fonts"
-mkdir -p $HOME/.local/share/fonts/MesloLGS
-wget -nc -P $HOME/.local/share/fonts/MesloLGS -i ./meslo-urls.txt
+mkdir -p "$HOME/.local/share/fonts/MesloLGS"
+wget -nc -P "$HOME/.local/share/fonts/MesloLGS" -i ./font_urls/meslo-urls.txt
 echo "Done: Downloading Terminal Fonts"
 
 echo "Installing pnpm"
-curl -fsSL https://get.pnpm.io/install.sh | env PNPM_VERSION=<version> sh -
+curl -fsSL https://get.pnpm.io/install.sh | sh -
 pnpm env use --global lts
 echo "Done: Installing pnpm"
 
 echo "Installing NeoVim"
 if [ -d ~/.config/nvim ]; then
-    mv ~/.config/nvim ~/.config/nvim.bak
+  mv ~/.config/nvim ~/.config/nvim.bak
+  mv ~/.local/share/nvim ~/.local/share/nvim.bak
+  mv ~/.local/state/nvim ~/.local/state/nvim.bak
 fi
-git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-nvim +PackerSync
-echo "Done: Installing NeoVim"
 
-# Install Antigen for ZSH
-echo "Installing Antigen for ZSH"
-curl -L git.io/antigen > ~/.config/antigen.zsh
-echo "Done: Installing Antigen for ZSH"
+if command -v "gh" &>/dev/null; then
+  echo "Cloning lazyvim config using github-cli"
+  gh repo clone ll931217/lazyvim_config ~/.config/nvim
+else
+  echo "Cloning lazyvim config using git"
+  git clone https://github.com/ll931217/lazyvim_config.git ~/.config/nvim
+fi
+echo "Done: Installing NeoVim"
 
 echo "Done with setting up environment"
