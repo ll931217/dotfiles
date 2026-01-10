2317591b1fbc2660031d9b32a75c3b50de41b646
Liang-Shih Lin <liangshihlin@gmail.com>
2026-01-10 20:02:47 +0800

    Remove disk-io bar

--- .config/polybar/scripts/disk-io.sh
+++ .config/polybar/scripts/disk-io.sh
@@ -15,34 +15,13 @@ $1 == dev {
     read_kbs = $3
     write_kbs = $4
 
-    read_bars = int(read_kbs / 50)
-    write_bars = int(write_kbs / 50)
-
-    if (read_bars > 7) read_bars = 7
-    if (write_bars > 7) write_bars = 7
-    if (read_bars < 0) read_bars = 0
-    if (write_bars < 0) write_bars = 0
-
-    read_bar_str = ""
-    write_bar_str = ""
-
-    for (i = 0; i < 7; i++) {
-        if (i < read_bars) read_bar_str = read_bar_str "█"
-        else read_bar_str = read_bar_str " "
-    }
-
-    for (i = 0; i < 7; i++) {
-        if (i < write_bars) write_bar_str = write_bar_str "█"
-        else write_bar_str = write_bar_str " "
-    }
-
-    printf "R:%3dK/s%s W:%3dK/s%s", int(read_kbs), read_bar_str, int(write_kbs), write_bar_str
+    printf "R:%3dK/s W:%3dK/s", int(read_kbs), int(write_kbs)
     exit
 }
 ')
 
 if [ -z "$OUTPUT" ]; then
-    echo "R:  0K/s       W:  0K/s      "
+  echo "R:  0K/s W:  0K/s"
 else
-    echo "$OUTPUT"
+  echo "$OUTPUT"
 fi
